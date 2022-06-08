6315aa7602a82d92885e1573884ce4a55833b985
Rajat Jain <rajat.apple007@gmail.com>
2022-06-07 19:14:23 -0700

    .gitignore

---
+++ src/ffmpeg.js
@@ -0,0 +1,20 @@
+// Generate .m3u8 and .ts files form the video
+
+const ffmpeg = require("fluent-ffmpeg");
+const ffmpegInstaller = require("@ffmpeg-installer/ffmpeg");
+
+ffmpeg.setFfmpegPath(ffmpegInstaller.path);
+ffmpeg("videos/video.mp4", { timeout: 432000 })
+  .addOptions([
+    "-profile:v baseline",
+    "-level 3.0",
+    "-start_number 0",
+    "-hls_time 10",
+    "-hls_list_size 0",
+    "-f hls",
+  ])
+  .output("videos/output.m3u8")
+  .on("end", () => {
+    console.log("End");
+  })
+  .run();
